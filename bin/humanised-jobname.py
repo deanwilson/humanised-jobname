36e5c8e098c3d2309f0f2db6b91ab85eed33d51b
Dean Wilson <dean.wilson@gmail.com>
2020-07-27 18:20:30 +0100

    Make flake run against the code base

--- bin/humanised-jobname.py
+++ bin/humanised-jobname.py
@@ -6,7 +6,7 @@ import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../lib"))
 
-from humanised_jobname import HumanisedJobname
+from humanised_jobname import HumanisedJobname  # noqa: E402
 
 if __name__ == "__main__":
 
