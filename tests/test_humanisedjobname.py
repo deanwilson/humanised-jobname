ec1bb369b6d4328db66acf911662db11d07311ba
Dean Wilson <dean.wilson@gmail.com>
2020-07-29 07:57:12 +0100

    Use pytest-datadir to run texts with known fixtures
    We add two files with one word each so we can have predicatable output
    to test against while still ensuring the loading process works

--- tests/test_humanisedjobname.py
+++ tests/test_humanisedjobname.py
@@ -5,8 +5,31 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "../lib"))
 
 from humanised_jobname import HumanisedJobname  # noqa: E402
 
-class TestClass:
-    def test_simple_object(self):
-        """ check the default seperate is in the object string representation """
-        job_name = HumanisedJobname()
-        assert "-" in str(job_name)
+def test_simple_object(datadir):
+    """ check the default seperate is in the object string representation """
+    job_name = HumanisedJobname()
+    assert "-" in str(job_name)
+
+
+def test_custom_separator_constructor(datadir):
+    job_name = HumanisedJobname(separator='===')
+
+    assert "===" in str(job_name)
+
+
+def test_custom_separator_method(datadir):
+    job_name = HumanisedJobname()
+
+    job_name.set_separator("%%%")
+
+    assert "%%%" in str(job_name)
+
+
+def test_constructor_custom_files(datadir):
+    adjectives = (datadir / "adjectives.yaml")
+    right_hand = (datadir / "fedora-names.yaml")
+
+    job_name = HumanisedJobname(adjective_file=adjectives, right_hand=right_hand)
+
+    assert "perfect-beefy_miracle" == str(job_name)
+
