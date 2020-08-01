6ae362b5e82afa8dc48b96781c3ef23ad1ade5e6
Dean Wilson <dean.wilson@gmail.com>
2020-08-01 21:15:38 +0100

    Added flask routes to show information about the data files

--- humanisedflask/__init__.py
+++ humanisedflask/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
+from pathlib import Path
 from prometheus_flask_exporter import PrometheusMetrics
 
 import sys
 import os
+import yaml
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../lib"))
 
@@ -35,6 +37,47 @@ def default():
         job_name.right_hand_file(right_hand)
 
     return str(job_name)
+
+
+@app.route("/source_names")
+def source_names():
+    output_format = request.args.get("format", "html")
+
+    if output_format == "text":
+        separator = "\n"
+    else:
+        separator = "<br>"
+
+    sources = sorted(list(Path("data").glob("*.yaml")))
+
+    names = [s.name for s in sources]
+
+    return separator.join(names)
+
+
+@app.route("/sources")
+def list_sources():
+
+    output_format = request.args.get("format", "html")
+
+    if output_format == "text":
+        separator = "\n"
+    else:
+        separator = "<br>"
+
+    sources = sorted(list(Path("data").glob("*.yaml")))
+    details = []
+
+    for source in sources:
+        with open(source) as f:
+            # TODO: Exception handling
+            yaml_content = yaml.load(f.read(), Loader=yaml.SafeLoader)
+            description = yaml_content["words"]["meta"]["description"]
+            count = len(yaml_content["words"]["words"])
+
+            details.append(f"{source.name}: {description} - {count} words")
+
+    return separator.join(details)
 
 
 metrics.register_default(
